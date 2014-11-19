35f9090be7805bdde6af8da000e7693ba14fb93b
Dmitry Mordvinov <dvvsrd@gmail.com>
2014-11-19 18:05:54 +0300

    Fixed printing on display in NXT mode

--- nxtOSEK/ecrobot/c/trik_studio_utils.c
+++ nxtOSEK/ecrobot/c/trik_studio_utils.c
@@ -2,6 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
+
+char *formatString(char *format, ...)
+{
+	char buffer[256];
+	va_list args;
+	va_start(args, format);
+	vsnprintf(buffer, 256, format, args);
+	va_end(args);
+	return buffer;
+}
 
 char *concat(char *str1, char *str2) {
 	char *result = (char *)malloc(strlen(str1) + strlen(str2));
@@ -10,23 +21,13 @@ char *concat(char *str1, char *str2) {
 }
 
 char *int_to_string(int i) {
-	int const maxLen = 11;
-	char *result = (char *)malloc(maxLen);
-	snprintf(result, maxLen, "%d", i);
-	return result;
+	return formatString("%d", i);
 }
 
-
 char *float_to_string(float f) {
-	int const maxLen = 15;
-	char *result = (char *)malloc(maxLen);
-	snprintf(result, maxLen, "%d.%d", (int)f, ((int)((f - (int)f) * 1000)));
-	return result;
+	return formatString("%d.%d", (int)f, ((int)((f - (int)f) * 1000)));
 }
 
 char *other_to_string(void *arr) {
-	int const maxLen = 14;
-	char *result = (char *)malloc(maxLen);
-	snprintf(result, maxLen, "0x%0", (int)arr);
-	return result;
+	return formatString("%p", arr);
 }
