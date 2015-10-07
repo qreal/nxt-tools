8f881963251140530c0008ce91f9159984278d7b
Dmitry Mordvinov <dvvsrd@gmail.com>
2015-10-07 19:52:40 +0300

    Implemented croping NXT program name to 15 symbols

--- upload.sh
+++ upload.sh
@@ -2,8 +2,8 @@ PATH=$PWD/cygwin/bin:$PATH
 cd $2
 make clean    
 make all
-echo Executing NeXTTool to upload $1_OSEK.rxe...
- $3/nexttool/NeXTTool.exe  /COM=usb -download=$1_OSEK.rxe
- $3/nexttool/NeXTTool.exe  /COM=usb -listfiles=$1_OSEK.rxe
+echo Executing NeXTTool to upload $1.rxe...
+ $3/nexttool/NeXTTool.exe  /COM=usb -download=$1.rxe
+ $3/nexttool/NeXTTool.exe  /COM=usb -listfiles=${1:0:15}.rxe # NXT crops file name to 15 letters, so doing it here too...
 echo "NeXTTool is terminated"
 echo "Done"
